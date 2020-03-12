b36169d294d1bb35d53ff68e63e45045c19af225
Idong Essien <idong.essien@comcast.net>
2020-03-12 19:40:48 -0400

    imported state and effect hooks, created func useDarkMode

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 export default function useLocalStorage(key, initialValue) {
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
