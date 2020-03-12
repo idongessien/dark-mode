e506b196207e905ac54ed94536f535084620f5be
Idong Essien <idong.essien@comcast.net>
2020-03-12 19:30:11 -0400

    Updated storedValue to value and added setvalue to returned array

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,11 +1,12 @@
 export default function useLocalStorage(key, initialValue) {
-    const [storedValue, setStoreValue] = useState(() => {
+    const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
     });
     
     function setValue(value) {
-        
+        window.localStorage.setItem(key, JSON.stringify(value));
+        setStoredValue(value);
     }
-    return [storedValue];
+    return [storedValue, setValue];
 };
