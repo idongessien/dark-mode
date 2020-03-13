0fca38067bcfa7747816c6fa3097df1a15c853a6
Idong Essien <idong.essien@comcast.net>
2020-03-12 21:20:14 -0400

    called the useLocalStorage custom hook

--- hooks/useDarkMode.js
+++ hooks/useDarkMode.js
@@ -2,5 +2,7 @@ import React, {useState, useEffect} from 'react'
 import useLocalStorage from './useLocalStorage'
 
 export default function useDarkMode(){
+ const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
 
+ 
 }
